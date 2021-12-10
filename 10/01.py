3e0f305cc599ea66807bd426de84c8deb4c95f7a
Apurva Nakade <apurvnakade@gmail.com>
2021-12-10 00:15:01 -0600

    solution to Day 10

---
+++ 10/01.py
@@ -0,0 +1,24 @@
+with open('input.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+illegal_chars = {
+  ')' : 3, 
+  ']' : 57, 
+  '}' : 1197, 
+  '>' : 25137}
+
+score = 0
+
+for line in lines:
+  stack = []
+  for char in line:
+    if char == '(' or char == '[' or char == '{' or char == '<':
+      stack.append(char)
+    elif char == ')' or char == ']' or char == '}' or char == '>':
+      top = stack.pop()
+      if (top == '(' and not char == ')') or (top == '[' and not char == ']') or (top == '{' and not char == '}') or (top == '<' and not char == '>'):
+        score += illegal_chars[char]
+        break
+
+print(score)
+    
---
+++ 10/02.py
@@ -0,0 +1,34 @@
+with open('input.txt') as f:
+    lines = [line.strip() for line in f.readlines()]
+
+illegal_chars = {
+  '(' : 1, 
+  '[' : 2, 
+  '{' : 3, 
+  '<' : 4}
+
+scores = []
+
+for line in lines:
+  illegal_line = False
+  stack = []
+  for char in line:
+    if char == '(' or char == '[' or char == '{' or char == '<':
+      stack.append(char)
+    elif char == ')' or char == ']' or char == '}' or char == '>':
+      top = stack.pop()
+      if (top == '(' and not char == ')') or (top == '[' and not char == ']') or (top == '{' and not char == '}') or (top == '<' and not char == '>'):
+        illegal_line = True
+        break
+  
+  if not illegal_line:
+    score = 0
+    for char in reversed(stack):
+      score = score * 5 + illegal_chars[char]
+    scores.append(score)
+
+scores.sort()
+print(len(scores)) #47
+print(scores[23])
+
+    
