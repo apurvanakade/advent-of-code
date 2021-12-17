f8cc7593ba033170d708b81e61c9d0cac7eddcfe
apurvnakade <apurvnakade@gmail.com>
2021-12-17 00:32:30 -0600

    better proof

--- 2021/17/01-02.py
+++ 2021/17/01-02.py
@@ -1,22 +1,27 @@
+import math
+
 with open('input.txt', 'r') as f:
-  target_x_left_end = int(f.readline().strip())
-  target_x_right_end = int(f.readline().strip())
-  target_y_top_end = int(f.readline().strip())
-  target_y_bottom_end = int(f.readline().strip())
+  left_end = int(f.readline().strip())
+  right_end = int(f.readline().strip())
+  top_end = int(f.readline().strip())
+  bottom_end = int(f.readline().strip())
 
 
 maximum_height = 0
-possible_velocities = set()
+num_possible_velocities = 0
 
-for v_x in range(target_x_right_end + 1):
-  for v_y in range(2*target_y_bottom_end, -2*target_y_bottom_end):
-    for time in range(1, 300):
-      y_at_time = (time) * v_y - time * (time - 1) /2
+for v_x in range(right_end + 1):
+  for v_y in range(2*bottom_end, -2*bottom_end):    
+    if (v_y + 1/2)**2 - 4*top_end < 0 or (v_y + 1/2)**2 - 4*top_end < 0:
+      continue
+    max_t = math.floor((v_y + 1/2) + math.sqrt((v_y + 1/2)**2 - 2*top_end))
+    min_t = math.ceil((v_y + 1/2) + math.sqrt((v_y + 1/2)**2 - 2*bottom_end))
+    for time in range(min_t, max_t + 1):
       t = min(time, v_x)
-      x_at_time = (t) * v_x - t * (t - 1) / 2
-
-      if x_at_time >= target_x_left_end and x_at_time <= target_x_right_end and y_at_time >= target_y_top_end and y_at_time <= target_y_bottom_end:
-        possible_velocities.add((v_x, v_y))
+      x_at_time = t * v_x - t * (t - 1) / 2
+      if x_at_time >= left_end and x_at_time <= right_end:
         maximum_height = max(maximum_height, 0, (v_y * (v_y + 1))/2)
+        num_possible_velocities += 1
+        break
 
-print('possible_velocities = ', len(possible_velocities), 'maximum_height = ', maximum_height)
+print('num_possible_velocities = ', num_possible_velocities, 'maximum_height = ', maximum_height)
